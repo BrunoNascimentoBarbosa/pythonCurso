b1609096ac956a1ee1c27766820301da882cb6a3
Bruno Nascimento <bruno@brunonascimento.com>
2022-08-31 10:09:45 -0300

    Corrigindo desafios

--- desafio_23.py
+++ desafio_23.py
@@ -10,11 +10,14 @@ CENTENA:8
 MILHAR:1
 '''
 
-n = input('Digite um número de 0 a 9999: ')
-print(n)
-n= (n.strip())
+num = int(input('Digite um número de 0 a 9999: '))
+u = num // 1 % 10
+d = num // 10 % 10
+c = num // 100 % 10
+m = num // 1000 % 10
 
-print('Unidade:'+(n[3]))
-print('DEZENA:'+(n[2]))
-print('CENTENA:'+(n[1]))
-print('MILHAR:' +(n[0]))
+print('Analisando o número {}'.format(num))
+print('Unidade:{}'.format(u))
+print('Dezena:{}'.format(d))
+print('Centena:{}'.format(c))
+print('Millhar:{}'.format(m))
--- desafio_24.py
+++ desafio_24.py
@@ -3,12 +3,9 @@ Crie um programa que leia o nome de uma cidade e diga
 se ela começa ou não com o nome "SANTO"
 '''
 
-city = input('Diginte o nome de uma cidade:')
-print(city)
+city = input('Diginte o nome de uma cidade:').strip()
+print(city[:5].upper() == 'SANTO')
+
 
 city = city.find('SANTO')
 
-if city != -1:
-    print('Tem SANTOS NO NOME.')
-else:
-    print('Não tem SANTOS NO NOME.')
