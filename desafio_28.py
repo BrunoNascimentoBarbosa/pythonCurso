110723e746f4eae9cb594fea7cf66eca2c88ffed
Bruno Nascimento <bruno@brunonascimento.com>
2022-09-03 10:25:50 -0300

    Ajuste desafio 28

--- desafio_28.py
+++ desafio_28.py
@@ -6,13 +6,15 @@ descobrir qual foi o número escolhido pelo computador.
 O programa deverá escrever na tela se o usuário venceu ou perdeu.
 '''
 import random
+from time import sleep
 
 print('O computador vai pensar um número de 0 a 5.')
 print('Você deve adivinhar o número para ganhar.')
 numero = random.randrange(5)
 
 jogada = int(input('Digite um número de 0 a 5: '))
-
+print('Processando...')
+sleep(3)
 if jogada == numero:
     print('Você ganhou o número escolhido foi :{}'.format(numero))
 else:
