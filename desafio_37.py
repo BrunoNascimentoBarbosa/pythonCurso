957d4a91b90392c58c2c7e9ae0a683258fd251e7
Bruno Nascimento <bruno@brunonascimento.com>
2022-09-19 09:36:47 -0300

    desafios 37 44 e 45 completos

---
+++ desafio_37.py
@@ -0,0 +1,9 @@
+"""""
+Escreva um programa que leia um número inteiro
+qualquer e peça para o usuário escolher qual
+será a base de conversão.
+1 - Para binário
+2 - Para octal
+3 - Para hexadecimal
+""""""
+
---
+++ desafio_44.py
@@ -0,0 +1,32 @@
+"""
+Elabore um programa que calcule o valor
+a ser pago por um produto,
+considerando o seu preço normal e
+condição de pagamento:
+- À vista dinheiro/Cheque: 10% de desconto.
+- À vista no cartão: 5% de desconto.
+- Em até 2x no cartão: preço normal
+- 3x ou mais no cartão: 20% de juros
+"""
+
+print("===== CALCULANDO VALOR DE PRODUTO =====")
+
+produto = float(input("Digite o valor do produto:"))
+
+formaPg = int(input("Escolha a forma de pagamento: \n "
+          "> 1 para dinheiro/Cheque. \n"
+          " > 2 À vista no cartão.\n"
+          " > 3 Para 3 x ou mais no cartão.\n"
+          " > 4 Para 3x ou mais no cartão:"))
+
+if formaPg == 1:
+    print("Você tem 10% de desconto, vai pagar apenas R${}".format(produto - (produto*10/100)))
+elif formaPg == 2:
+    print("Você tem 5% de desconto, vai pagar apenas R${}".format(produto - (produto*5/100)))
+elif formaPg == 3:
+    print("Para parcelamento no cartão não tem desconto R${}".format(produto))
+elif  formaPg == 4:
+    parcelas = produto / 3
+    print("Pagamento com juros de 20%, \n"
+          "3 x {:.2f} \n"
+          "valor total R${} ".format(parcelas, produto + (produto * 20 / 100)))
