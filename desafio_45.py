ff629da9eb49f58f46df5c50eb9f1248a4096dd7
Bruno Nascimento <bruno@brunonascimento.com>
2022-09-19 10:07:38 -0300

    Desafio 45 completo

--- desafio_45.py
+++ desafio_45.py
@@ -4,3 +4,32 @@ jogar jokenpô com você.
 Pedra, papel e tesoura.
 """
 
+import random
+
+print("Vamos Jogar JOKENPÔ")
+print ("Escolha:\n 1 Pedra "
+       "\n 2 PAPEL \n"
+       " 3 TESOURA ")
+jogador = int(input("Digite sua jogada:"))
+jogadas = [1,2,3]
+maquina = (random.choice(jogadas))
+print(jogador,maquina)
+
+if jogador == 1 and maquina == 3:
+    print("Você venceu :)")
+elif jogador == 1 and maquina ==1:
+    print("Empate")
+elif jogador == 1 and maquina == 2:
+    print("Você perdeu e a maquina ganhou.")
+elif jogador == 2 and maquina == 1:
+    print("Você venceu :)")
+elif jogador == 2 and maquina == 2:
+    print("Empate")
+elif jogador == 2 and maquina == 3:
+    print("Você perdeu e maquina ganhou")
+elif jogador == 3 and maquina == 2:
+    print("Você venceu :)")
+elif jogador == 3 and maquina == 1:
+    print("Você perdeu e maquina ganhou")
+elif jogador == 3 and maquina == 3:
+    print("Empate")
