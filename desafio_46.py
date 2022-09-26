ae9aa628477c9494192749624f411504edeec8f2
Bruno Nascimento <bruno@brunonascimento.com>
2022-09-26 10:20:36 -0300

    desafio 46 completo

---
+++ desafio_46.py
@@ -0,0 +1,12 @@
+'''
+Faça um programa que mostre na tela uma contagem regressiva para estouro de
+fogos de artifício, indo de 10 até 0, com uma pausa de 1 segundo entre eles.
+'''
+
+from time import sleep
+print('Contagem regressiva')
+
+for c in range(10,0,-1):
+    print(c)
+    sleep(1)
+print("🎆 "* 10)
