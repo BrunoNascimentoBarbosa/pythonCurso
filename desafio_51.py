42c68f118147279b58178a5a4cda4dd0a83ba0f1
Bruno Nascimento <bruno@brunonascimento.com>
2022-09-28 10:57:16 -0300

    desafio 51 completo

--- desafio_51.py
+++ desafio_51.py
@@ -4,3 +4,9 @@ termo e a razão de uma PA. No final, mostre
 os 10 primeiros termos dessa progressão.
 '''
 
+termo = int(input("Digite o primeiro termo dessa PA: "))
+razao = int(input("Escolha a razão da sua PA. Ex: 2,3,4,7,8, 10 : "))
+soma = 0
+for c in range(termo,(termo+10)):
+    soma = soma + razao
+    print(soma  ,end=" | ")
