57117fe76219bae984c7be79adaa37d48652b0a5
Bruno Nascimento <bruno@brunonascimento.com>
2022-09-29 11:29:53 -0300

    desafio 55 completo

--- desafio_55.py
+++ desafio_55.py
@@ -4,3 +4,18 @@ que leia oo peso de
 cinco pessoas. No final, mostre
 qual foi o maior e o menor peso lidos.
 '''
+maior = 0
+maneor = 0
+
+for p in range (1, 6):
+    peso = float(input("Digite o peso: "))
+    if p == 1:
+        maior = peso
+        menor = peso
+    else:
+        if peso > maior:
+            maior = peso
+        if peso < menor:
+            menor = peso
+print('O maior peso lido foi de {}kg'.format(maior))
+print('O menor peso lido foi de {}kg'.format(menor))
